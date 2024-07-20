1a4ec7c478368368d8fad5abe602ae52def88551
Olga Liubar <olgaliubar@Olgas-MacBook-Air.local>
2024-07-21 00:00:03 +0300

    refactor routing to use createBrowserRouter

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from '../layout/layout';
 import Main from '../../pages/main/main';
 import Favorites from '../../pages/favorites/favorites';
@@ -24,37 +24,41 @@ export default function App({
   comments,
   offersCount,
 }: AppProps): JSX.Element {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={APP_ROUTE.ROOT} element={<Layout />}>
-          <Route
-            index
-            element={<Main offersCount={offersCount} offers={offers} />}
-          />
-          <Route
-            path={APP_ROUTE.FAVORITES}
-            element={
-              <PrivateRoute status={currentStatus}>
-                <Favorites favorites={favorites} />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={APP_ROUTE.OFFER}
-            element={<Offer comments={comments} />}
-          />
-        </Route>
-        <Route
-          path={APP_ROUTE.LOGIN}
-          element={
+  const router = createBrowserRouter([
+    {
+      path: APP_ROUTE.ROOT,
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Main offersCount={offersCount} offers={offers} />,
+        },
+        {
+          path: APP_ROUTE.FAVORITES,
+          element: (
+            <PrivateRoute status={currentStatus}>
+              <Favorites favorites={favorites} />
+            </PrivateRoute>
+          ),
+        },
+        {
+          path: APP_ROUTE.OFFER,
+          element: <Offer comments={comments} />,
+        },
+        {
+          path: APP_ROUTE.LOGIN,
+          element: (
             <PublicRoute status={currentStatus}>
               <Login />
             </PublicRoute>
-          }
-        />
-        <Route path={APP_ROUTE.NOT_FOUND} element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+          ),
+        },
+        {
+          path: APP_ROUTE.NOT_FOUND,
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
