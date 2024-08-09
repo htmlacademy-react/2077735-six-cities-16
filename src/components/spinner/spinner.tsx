590deefa6da1c32f33970206fad48f73a52a47a9
Olga Liubar <olgaliubar@Olgas-MacBook-Air.local>
2024-08-09 19:09:14 +0300

    add spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,11 @@
+import './spinner.css';
+
+export default function Spinner() {
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
+}
