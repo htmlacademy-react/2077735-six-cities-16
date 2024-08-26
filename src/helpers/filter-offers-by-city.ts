e204352482f03a3a203746541d3fbd618480d371
Olga Liubar <olgaliubar@Olgas-MacBook-Air.local>
2024-08-26 15:18:36 +0300

    delete unused helpers

--- src/helpers/filter-offers-by-city.ts
+++
@@ -1,5 +0,0 @@
-import { Offer } from '../types';
-
-export function filterOffersByCity(offers: Offer[], cityName: string) {
-  return offers.filter((offer) => offer.city.name === cityName);
-}
--- src/helpers/filter-offers-by-fav.ts
+++
@@ -1,5 +0,0 @@
-import { Offer } from '../types';
-
-export function filterOffersByFav(offersArray: Offer[]) {
-  return offersArray.filter((offer) => offer.isFavorite);
-}
